44a2f5ad2e7d85fe4dbb8f14824ffa0e27af7119
wu123 <535504958@qq.com>
2023-04-20 11:04:43 +0800

    create pod_controller.go

--- k8s-operator/main.go
+++ k8s-operator/main.go
@@ -98,6 +98,15 @@ func main() {
 	}
 	//+kubebuilder:scaffold:builder
 
+	// Endpoint Controller
+	if err = (&controllers.PodReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "podReconciler")
+		os.Exit(1)
+	}
+
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
 		setupLog.Error(err, "unable to set up health check")
 		os.Exit(1)
